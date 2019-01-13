748562c951154f52891187166679248914ffa1ba
donkey_hotei <donkey-hotei@cryptolab.net>
2019-01-12 18:26:48 -0800

    solve day five part two

--- adventofcode/2018/day-five/src/main.rs
+++ adventofcode/2018/day-five/src/main.rs
@@ -25,28 +25,26 @@ impl Polymer {
     }
 
     fn react(&self) -> Polymer {
-        let units = self.units
-            .iter()
-            .fold(
-                Vec::with_capacity(self.len() / 2),
-                |mut v: Vec<Unit>, &a| {
+        Polymer {
+            units: self.units
+                .iter()
+                .fold(
+                    Vec::with_capacity(self.len() / 2),
+                    |mut v: Vec<Unit>, &a| {
 
-                    if let Some(b) = v.last() {
-                        if a.reacts_with(b) {
-                            v.pop();
-                            return v
+                        if let Some(b) = v.last() {
+                            if a.reacts_with(b) {
+                                v.pop();
+                                return v
+                            }
                         }
-                    }
 
-                    v.push(a);
-                    v
-                });
-
-        Polymer { units }
+                        v.push(a);
+                        v
+                    })
+        }
     }
 }
-
-
 
 impl FromStr for Polymer {
     type Err = ParseError;
@@ -62,9 +60,24 @@ impl FromStr for Polymer {
 }
 
 fn main() {
-    let polymer = include_str!("../input.txt").trim();
+    let input = include_str!("../input.txt").trim();
 
-    if let Ok(polymer) = polymer.parse::<Polymer>() {
-        println!("{:?}", polymer.react().len())
+    if let Ok(polymer) = input.parse::<Polymer>() {
+        // part one
+        println!("{:?}", polymer.react().len());
+
+        // part two
+        let shortest_polymer = (b'A'..=b'Z')
+            .map(|letter| {
+                input
+                    .replace(letter as char, "")
+                    .replace((letter as char).to_ascii_lowercase(), "")
+                    .parse::<Polymer>()
+                    .unwrap()
+                    .react()
+            })
+            .min_by_key(|polymer| polymer.len());
+
+        println!("{:?}", shortest_polymer.unwrap().react().len());
     }
 }

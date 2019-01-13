fd8faa23d3e999fff971425e082e6b6c4fd39fc3
donkey_hotei <donkey-hotei@cryptolab.net>
2019-01-12 16:43:58 -0800

    complete day three

--- adventofcode/2018/day-three/src/main.rs
+++ adventofcode/2018/day-three/src/main.rs
@@ -1,57 +1,111 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::fs;
 use regex::Regex;
+use std::str::FromStr;
+use std::error::Error;
+use std::collections::HashMap;
+
+struct Tile {
+    id: usize,
+    left: usize,
+    top: usize,
+    width: usize,
+    height: usize,
+}
+
+impl Tile {
+    fn iter_points(&self) -> IterPoints {
+        IterPoints {
+            tile: self,
+            x: self.left,
+            y: self.top,
+        }
+    }
+}
+
+struct IterPoints<'t> {
+    tile: &'t Tile,
+    x: usize,
+    y: usize,
+}
+
+impl<'t> Iterator for IterPoints<'t> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y >= self.tile.top + self.tile.height {
+            self.y = self.tile.top;
+            self.x += 1;
+        }
+
+        if self.x >= self.tile.left + self.tile.width {
+            return None;
+        }
+
+        let (x, y) = (self.x, self.y);
+
+        self.y += 1;
+
+        Some((x, y))
+    }
+}
+
+// point maps to number of overlapping tiles at that point
+type Grid = HashMap<(usize, usize), usize>;
+
+impl FromStr for Tile {
+    type Err = Box<Error>;
+
+    fn from_str(s: &str) -> Result<Tile, Box<Error>> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"#(?P<id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)")
+                .unwrap();
+
+        }
+
+        let caps = match RE.captures(s) {
+            Some(caps) => caps,
+            None => return Err(Box::<Error>::from(format!("failure to parse claim"))),
+        };
+
+
+        Ok(Tile {
+            id: caps["id"].parse()?,
+            top: caps["top"].parse()?,
+            left: caps["left"].parse()?,
+            width: caps["width"].parse()?,
+            height: caps["height"].parse()?,
+        })
+    }
+}
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("error reading file");
-    let regex = Regex::new(
-        r"(#\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)")
-        .unwrap();
 
-    let mut matrix = [[0u8; 1000]; 1000];
+    let mut tiles: Vec<Tile> = Vec::new();
 
     for line in input.lines() {
-        for cap in regex.captures_iter(&line) {
-            let left = &cap
-                .name("left")
-                .unwrap()
-                .as_str()
-                .parse::<usize>()
-                .unwrap();
-            let top = &cap
-                .name("top")
-                .unwrap()
-                .as_str()
-                .parse::<usize>()
-                .unwrap();
-            let width = &cap
-                .name("width")
-                .unwrap()
-                .as_str()
-                .parse::<usize>()
-                .unwrap();
-            let height = &cap
-                .name("height")
-                .unwrap()
-                .as_str()
-                .parse::<usize>()
-                .unwrap();
+        tiles.push(line.parse().unwrap());
+    }
 
-            for i in *left..left + width {
-                for j in *top..top + height {
-                    matrix[i][j] += 1;
-                }
-            }
+    let mut grid = Grid::new();
+
+    for tile in &tiles {
+        for (x, y) in tile.iter_points() {
+            *grid.entry((x, y)).or_default() += 1;
         }
     }
 
-    let mut overclaims = 0;
-    for i in 0..1000 {
-        for j in 0..1000 {
-            if matrix[i][j] > 1 {
-                overclaims += 1;
-            }
+    let count = grid.values().filter(|&&count| count > 1).count();
+    println!("{:?}", count);
+
+
+    for tile in tiles {
+        if tile.iter_points().all(|p| grid[&p] == 1) {
+            println!("{:?}", tile.id);
         }
     }
-
-    println!("{:?}", overclaims)
 }

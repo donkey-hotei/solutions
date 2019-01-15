21d596caeef0dad31ec69a413a64650e84c54a04
donkey_hotei <donkey-hotei@cryptolab.net>
2019-01-15 15:41:06 -0800

    solve leetcode #304 with row caching

--- leetcode/304.rs
+++ leetcode/304.rs
@@ -1,18 +1,48 @@
 struct NumMatrix {
-    matrix: Vec<Vec<i32>>,
+    cache:  Vec<Vec<i32>>,
 }
-
 
 impl NumMatrix {
     fn new(matrix: Vec<Vec<i32>>) -> Self {
-        NumMatrix { matrix }
+        let (m, n) = (matrix.len(), matrix[0].len());
+
+        if m == 0 || n == 0 { return NumMatrix { cache: vec![] } }
+
+        let mut cache = vec![vec![0; n + 1]; m];
+        for x in (0..m) {
+            for y in (0..n) {
+                cache[x][y + 1] = cache[x][y] + matrix[x][y];
+            }
+        }
+
+        println!("{:?}", cache);
+
+        NumMatrix { cache }
     }
 
     fn sum_region(&self, row1: i32, col1: i32, row2: i32, col2: i32) -> i32 {
-        (row1..=row2)
-            .flat_map(|x| (col1..=col2).map(move |y| (x, y)))
-            .fold(0, |acc, (x, y)| {
-                acc + self.matrix[x as usize][y as usize]
-            })
+        if self.cache.len() == 0 { return 0; }
+
+        (row1..=row2).fold(0, |acc, row| {
+            acc +
+                self.cache[row as usize][col2 as usize + 1] -
+                self.cache[row as usize][col1 as usize]
+        })
     }
+}
+
+fn main() {
+    let matrix = vec![
+        vec![3, 0, 1, 4, 2],
+        vec![5, 6, 3, 2, 1],
+        vec![1, 2, 0, 1, 5],
+        vec![4, 1, 0, 1, 7],
+        vec![1, 0, 3, 0, 5]
+    ];
+
+    // let empty_matrix = vec![vec![]];
+
+    let obj = NumMatrix::new(matrix);
+
+    println!("{:?}", obj.sum_region(2, 1, 4, 3));
 }

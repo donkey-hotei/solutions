0a14b28b8092ac3ed94af07ef6f27694db042c5b
donkey-hotei <donkey_hotei@cryptolab.net>
2019-01-29 05:50:20 -0800

    condensify raindrops solution

--- exercism/rust/raindrops/src/lib.rs
+++ exercism/rust/raindrops/src/lib.rs
@@ -1,25 +1,17 @@
 pub fn raindrops(n: u32) -> String {
-    if n % 3 != 0 && n % 5 != 0 && n % 7 != 0 {
-        return n.to_string()
+    if n % 3 != 0 &&
+       n % 5 != 0 &&
+       n % 7 != 0 { n.to_string() }
+    else {
+        (1..=n)
+            .filter(|x| n % x == 0)
+            .fold(String::new(), |mut s, factor| {
+                match factor {
+                    3 => { s.push_str("Pling"); s },
+                    5 => { s.push_str("Plang"); s },
+                    7 => { s.push_str("Plong"); s },
+                    _ => s
+                }
+            })
     }
-
-    (1..=n)
-        .filter(|x| n % x == 0)
-        .fold(String::new(), |mut string, factor| {
-            match factor {
-                3 => {
-                    string.push_str("Pling");
-                    string
-                },
-                5 => {
-                    string.push_str("Plang");
-                    string
-                },
-                7 => {
-                    string.push_str("Plong");
-                    string
-                },
-                _ => string
-            }
-        })
 }

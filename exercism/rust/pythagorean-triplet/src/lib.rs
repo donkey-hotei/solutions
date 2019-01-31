59a805dc0857639a4f99f354433a78a05bf2584d
donkey_hotei <donkey-hotei@cryptolab.net>
2019-01-30 19:29:00 -0800

    solve pythagorean triplets in O(n) time and parallelize

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -1,9 +1,17 @@
 use std::collections::HashSet;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 pub fn find(n: u32) -> HashSet<[u32; 3]> {
-    (0..n / 3).flat_map(|a| (a + 1..n / 2).map(move |b| (a, b)))
-        .map(|(a, b)| (a, b, n - a - b))
-        .filter(|&(a, b, c)| a*a + b*b == c*c)
-        .map(|(a, b, c)| [a, b, c])
-        .collect()
+    (0..n / 3).into_par_iter().map(|a| {
+        let b = ((n - a) * (n - a) - a * a) / (2 * (n - a));
+        let c = n - a - b;
+
+        if a < b && b < c && a * a + b * b == c * c && a + b + c == n {
+            Some([a, b, c])
+        } else {
+            None
+        }
+    })
+    .filter_map(|x| x)
+    .collect()
 }

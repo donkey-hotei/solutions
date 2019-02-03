5e4f4b92b27e1a8b79339c68a4d20f2220720882
donkey_hotei <donkey-hotei@cryptolab.net>
2019-02-02 20:12:14 -0800

    use .pow()

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -5,11 +5,11 @@ pub fn find(n: u32) -> HashSet<[u32; 3]> {
     (0..n / 3)
         .into_par_iter()
         .map(|a| {
-            let b = ((n - a) * (n - a) - a * a) / (2 * (n - a));
+            let b = ((n - a).pow(2) - a.pow(2)) / (2 * (n - a));
             let c = n - a - b;
 
-            if a < b && b < c         &&
-               a * a + b * b == c * c &&
+            if a < b && b < c                  &&
+               a.pow(2) + b.pow(2) == c.pow(2) &&
                a + b + c == n { Some([a, b, c]) }
             else {
                 None
